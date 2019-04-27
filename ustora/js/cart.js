a71b4677c70fd731ea47481ad134cf0da8425b0b
Nguyễn Minh Duy <minhduynguyen10@gmail.com>
2019-04-27 19:39:40 +0700

    tương tự bảng dưới  - Minh Duy

--- ustora/js/cart.js
+++ ustora/js/cart.js
@@ -1,7 +1,7 @@
 var Bills = [
 	{
 		user: "duynm619",
-		products: ["Chào Mừng Đến NHK",],
+		products: ["Chào Mừng Đến NHK","5 Centimet Trên giây"],
 		number: [2,3],
 		// products: {"Chào Mừng Đến NHK":2, "5 Centimet Trên giây":3}
 		deal: false
