59076d212c4fee22edeb1d7483137b529cfcd20b
Nguyễn Minh Duy <minhduynguyen10@gmail.com>
2019-04-27 17:40:29 +0700

    t đã code xong phần xử lý giỏ hàng(nút thêm hàng, xóa hàng, thay đổi số lượng)
    Minh Duy

--- ustora/function.js
+++ ustora/function.js
@@ -110,7 +110,7 @@ function showLike (index, cnt = 4){
                         </div>  `;
                         if(data[i].TT == true){
                        dssp +=` <div class="product-option-shop ">
-                            <button id = "myBtn" href = "#" class="btn btn-outline-secondary bynow btn-block changecolor " data-idpr="${data[i].id}" onclick = "getDataIdpr(${data[i].id})">Mua Ngay</button>
+                            <button id = "myBtn" href = "#" class="btn btn-outline-secondary bynow btn-block changecolor " data-idpr="${data[i].id}" onclick = "addElement('${data[i].tenSP}')">Mua Ngay</button>
                         </div>  ` ;
                     }   
                          else {
@@ -145,18 +145,124 @@ function productLeft (index = 0, cnt = 4) {
   }
   ListLeft.innerHTML=ans;
 }
-  function recemtPost (cnt = 4) {
-    var ans = "<ul>";
-    for(var i = 0; i < data.length && cnt > 0; i++)
-    {
-      cnt--;
-      ans += `
-          <li><a href="">${data[i].tenSP}</a></li>
-      `
-    }
-    ans+="</ul>"
-    RecentPost.innerHTML=ans;
+function recemtPost (cnt = 4) {
+  var ans = "<ul>";
+  for(var i = 0; i < data.length && cnt > 0; i++)
+  {
+    cnt--;
+    ans += `
+        <li><a href="">${data[i].tenSP}</a></li>
+    `
   }
+  ans+="</ul>"
+  RecentPost.innerHTML=ans;
+}
+
+function showBill (user = "duynm619") {
+  var current = Bills.find(function (bill) {return bill.user == user && bill.deal == false;});
+  flag = true;
+  for (var i = 0; i < localStorage.length; i++)
+      if (localStorage[localStorage.key(i)] != -1){ flag = false; break;}
+  if (typeof current === "undefined" || (flag && localStorage.length != 0)){
+    CartShow.innerHTML = `<tr><td colspan = 6><h2>Bạn chưa thêm gì vào giỏ hàng cả.<br><a href="shop.html">Mua hàng nào</a><br><a href="#">Xem lịch sử giao dịch</a></h2></td></tr>`;
+    return;
+  }
+  var ans = "";
+  var Total = 0;
+  if (localStorage.length == 0)
+    for(var i = 0; i < current.products.length; i++)
+    {
+      if (localStorage.getItem(current.products[i]) == null)
+        localStorage.setItem(current.products[i],current.number[i]);
+    }
+  for(var i = 0; i < localStorage.length; i++)
+  {
+    var val = data.find(function (book) {return book.tenSP == localStorage.key(i)});
+    Total+=Math.min(val.sale,val.gia)*localStorage[localStorage.key(i)];
+    ans+=`
+            <tr class="cart_item">
+              <td class="product-remove">
+                  <input type="button" title="Remove this item" value="X" onclick='delElement("${val.tenSP}")'>
+              </td>
+              <td class="product-thumbnail">
+                  <a href="#"><img width="145" height="145" alt="poster_1_up" class="shop_thumbnail" src="${val.img}"></a>
+              </td>
+
+              <td class="product-name" width="30%" id - ${val.tenSP}.replace(' ','')>
+                  <a href="#">${val.tenSP}</a> 
+              </td>
+
+              <td class="product-price">
+                  <span class="amount">${Math.min(val.sale,val.gia)} VNĐ</span> 
+              </td>
+
+              <td class="product-quantity">
+                  <div class="quantity buttons_added">
+                      <input type="button" class="minus" value="-" onclick='minusNumberElement("${val.tenSP}")'>
+                      <input type="number" size="4" class="input-text qty text" title="Qty" value="${localStorage[localStorage.key(i)]}" min="0" step="1">
+                      <input type="button" class="plus" value="+" onclick='plusNumberElement("${val.tenSP}")'>
+                  </div>
+              </td>
+
+              <td class="product-subtotal">
+                  <span class="amount">${localStorage[localStorage.key(i)]*Math.min(val.sale,val.gia)} VND</span> 
+              </td>
+            </tr>
+         `;
+  }
+  CartShow.innerHTML = ans;
+  STotal.innerHTML = Total+" VNĐ";
+  // document.getElementsByClassName("cart-amunt").textContent = Total+" VNĐ";
+  document.getElementById("TotalMoney").innerHTML = Total+" VNĐ";
+  document.getElementById("NumProduct").innerHTML = localStorage.length;
+}
+
+function plusNumberElement (val, user = "duynm619") {
+  // var val = "5 Centimet Trên giây", user = "duynm619";
+  var currentBill = Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false });
+  var currentProduct =  Bills[currentBill].products.findIndex(function (valid) { return valid == val; });
+  localStorage.setItem(val, +localStorage.getItem(val)+1);
+  Bills[currentBill].number[currentProduct]++;
+  // Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].number[Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].products.findIndex(function (valid) { return valid == val; })]++;
+  showBill(user);
+}
+
+function minusNumberElement (val, user = "duynm619") {
+  var currentBill = Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false });
+  var currentProduct =  Bills[currentBill].products.findIndex(function (valid) { return valid == val; });
+  localStorage.setItem(val, Math.max(+localStorage.getItem(val)-1,1));
+  Bills[currentBill].number[currentProduct] = Math.max(Bills[currentBill].number[currentProduct]-1,0);
+  // Bills[currentBill].number[currentProduct] = Math.max(Bills[currentBill].number[currentProduct]-1, 0);
+  // Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].number[Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].products.findIndex(function (valid) { return valid == val; })] = Math.max(Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].number[Bills[Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false })].products.findIndex(function (valid) { return valid == val; })]-1, 0);
+  showBill(user);
+}
+
+function delElement (val, user = "duynm619") {
+  var currentBill = Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false });
+  var currentProduct =  Bills[currentBill].products.findIndex(function (valid) { return valid == val; });
+  localStorage.setItem(val, -1);
+  Bills[currentBill].number[currentProduct] = -1;
+  Bills[currentBill].products.splice(currentProduct, 1);
+  // localStorage.setItem(val,-1);
+  // localStorage.removeItem(val);
+
+  // console.log(val,currentBill,currentProduct);
+  showBill(user);
+}
+
+function addElement (val, user = "duynm619") {
+  var currentBill = Bills.findIndex(function (valid) { return valid.user == user && valid.deal == false });
+  var currentProduct =  Bills[currentBill].products.findIndex(function (valid) { return valid == val; });
+  if (currentProduct != -1)
+    plusNumberElement(val,user);
+  else
+  {
+    localStorage.setItem(val, 1);
+    Bills[currentBill].products.push(val);
+    Bills[currentBill].number.push(1);
+  }
+  showBill(user);
+}
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // phần code của Đạt
---
+++ ustora/js/cart.js
@@ -0,0 +1,10 @@
+var Bills = [
+	{
+		user: "duynm619",
+		products: ["Chào Mừng Đến NHK",],
+		number: [2,3],
+		// products: {"Chào Mừng Đến NHK":2, "5 Centimet Trên giây":3}
+		deal: false
+	}
+	
+];
