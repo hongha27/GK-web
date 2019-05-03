1384d394c3df1282f2fcd5dcb5934c99155d4f71
hà <tranthihongha321@gmail.com>
2019-05-04 01:06:19 +0700

    update

--- ustora/js/cart.js
+++ ustora/js/cart.js
@@ -1,18 +1,20 @@
 var Bills = [
 	{
-		id: "1",
+		id: "#1",
 		user: "duynm619",
-		products: ["1","2"],
-		number: [2,3],
+		products: ["1","2","5"],
+		number: [2,3,2],
+		coupon: "50%",
 		// products: {"Chào Mừng Đến NHK":2, "5 Centimet Trên giây":3}
 		deal: false
 	},
 	
 	{
-		id: "2",
+		id: "#2",
 		user: "duynm619",
 		products: ["9","12"],
 		number: [3,5],
+		coupon:"20000",
 		// products: {"Chào Mừng Đến NHK":2, "5 Centimet Trên giây":3}
 		deal: true
 	}
